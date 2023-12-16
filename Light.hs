ed7a5a606aee467e439b2c0067ec25321bf6cd1f
isabellewalford <issywalf@gmail.com>
2023-12-16 18:16:13 +0000

    optimizing

--- Light.hs
+++ Light.hs
@@ -1,3 +1,4 @@
+
 import Data.List
 
 generator1 :: [(Int,Int,Int,Int)]
@@ -9,11 +10,16 @@ generator1 = [(hr,mn,dy,mt)
     , dy <- [1..days]
     ]
 
-daysOfMonth :: Int -> Int 
-daysOfMonth m 
-    | m == 2 = 28
-    | m `elem` [4,6,9,11] = 30
-    | otherwise = 31
+daysOfMonth :: Int -> Int
+daysOfMonth = memoize daysOfMonth'
+    where
+    daysOfMonth' 2 = 28
+    daysOfMonth' m
+      | m `elem` [4, 6, 9, 11] = 30
+      | otherwise = 31
+
+memoize :: (Int -> a) -> (Int -> a)
+memoize f = (map f [0 ..] !!)
 
 magic :: (Int,Int,Int,Int) -> Bool
 magic (hr,mn,dy,mt) = 
@@ -31,51 +37,46 @@ factorable f n
 
 digits :: Int -> [Int]
 digits x  
-    | x < 10 = [0] ++ [x] 
-    | otherwise = [x `div` 10] ++ [x `mod` 10]
+    | x < 10 = [0, x] 
+    | otherwise = [x `div` 10, x `mod` 10]
 
-nodups :: [Int] -> Bool
-nodups s = 
-    s == nub s 
+
+nodups :: (Eq a, Ord a) => [a] -> Bool
+nodups = nodups' . sort
+  where
+    nodups' [] = True
+    nodups' [_] = True
+    nodups' (x1 : x2 : xs) = x1 /= x2 && nodups' (x2 : xs)
 
 segment :: Int -> Int
-segment(0) = 6
-segment(1) = 2
-segment(2) = 5
-segment(3) = 5
-segment(4) = 4
-segment(5) = 5
-segment(6) = 6
-segment(7) = 3
-segment(8) = 7
-segment(9) = 6
+segment = ([6, 2, 5, 5, 4, 5, 6, 3, 7, 6] !!)
 
 segments :: (Int,Int,Int,Int) -> Int
 segments (hr,mn,dy,mt) =
     sum segmentList
     where
-    digitList = digits hr ++ digits mn ++ digits dy ++ digits mt
+    digitList = digits hr ++ digits mn ++ digits dy ++ digits mt 
     segmentList = map segment digitList
 
 check :: (Int,Int,Int,Int) -> (Int,Int,Int,Int)
 check (hr,mn,dy,mt) 
     | mn < 60 = (hr,mn,dy,mt) 
-    | otherwise = hour(hr+1,0,dy,mt)
+    | otherwise = hour(hr+1,mn-60,dy,mt)
 
 hour :: (Int,Int,Int,Int) -> (Int,Int,Int,Int)
 hour (hr,mn,dy,mt) 
     | hr < 24 = (hr,mn,dy,mt) 
-    | otherwise = day(0,mn,dy+1,mt)
+    | otherwise = day(hr-24,mn,dy+1,mt)
 
 day :: (Int,Int,Int,Int) -> (Int,Int,Int,Int)
 day (hr,mn,dy,mt) 
     | dy <= daysOfMonth mt = (hr,mn,dy,mt) 
-    | otherwise = day(hr,mn,1,mt+1)
+    | otherwise = day(hr,mn,dy-(daysOfMonth mt),mt+1)
     
 month :: (Int,Int,Int,Int) -> (Int,Int,Int,Int)
 month (hr,mn,dy,mt) 
-    | mt < 13 = (hr,mn,dy,mt) 
-    | otherwise = day(hr,mn,dy,1)
+    | mt <= 12 = (hr,mn,dy,mt) 
+    | otherwise = day(hr,mn,dy,mt-12)
 
 tester1 :: (Int,Int,Int,Int) -> Bool
 tester1 (hr,mn,dy,mt) =
