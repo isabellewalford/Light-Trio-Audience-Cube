8c2db884176edc6fc5106755d5b09b09f491c6b0
isabellewalford <issywalf@gmail.com>
2023-12-19 12:43:53 +0000

    trio

--- Trio.hs
+++ Trio.hs
@@ -1,44 +1,41 @@
 import Data.List 
 
-generate3digit :: [Int]
-generate3digit = filter noZero [111..999]
-
-noZero :: Int -> Bool
-noZero n = '0' `notElem` show n
+combinations3digit :: [Int]
+combinations3digit = [123,124,125,126,127,128,129,
+    134,135,136,137,138,139,
+    145,146,147,148,149,
+    156,157,158,159,
+    167,168,169,
+    178,179,
+    189]
 
 digits :: Int -> [Int]
 digits 0
     = []
 digits x = digits (x `div` 10) ++ [x `mod` 10]
 
-number :: [Int] -> Int
-number [] = 0
-number (x:xs) 
-    = x * 10 ^ (length (x:xs) -1) + number xs
-
-genTuples :: Int -> [(Int,Int,Int,Int,Int)]
-genTuples x = [(n1,n2,n3,n4,n5)
-    | n1 <- map read (permutations (show x)) :: [Int]
-    , n2 <- map number 2digitperms 
-    , n3 <- map read (permutations (show x)) :: [Int]
-    , n4 <- map number 2digitperms
-    , n5 <- map read (permutations (show x)) :: [Int]
-    ] 
+genTuples :: [Int] -> [([Char], [Char], [Char], [Char], [Char])]
+genTuples [] = []
+genTuples (x:xs) = [(n1,n2,n3,n4,n5)
+    | n1 <- permutations (show x) 
+    , n2 <- twoDigitPerms 
+    , n3 <- permutations (show x)
+    , n4 <- twoDigitPerms
+    , n5 <- permutations (show x)
+    ] ++ genTuples xs 
     where 
-        numList = digits n
-        2digitPerms = addListsOfLists (permutations [head numList, numList !! 1]) (permutations [tail numList]) (permutations [head numList, numList !! 2])   
+        numList = digits x
+        n1 = show (head numList)
+        n2 = show (numList !! 1)
+        n3 = show (numList !! 2)
+        twoDigitPerms = (permutations (n1++n2)) ++ (permutations (n1++n3)) ++ (permutations (n2++n3))
 
-
-
--- Example usage
-main :: IO ()
-main = 
-  print  (take 100 generator3)
-
+generator2 :: [([Char], [Char], [Char], [Char], [Char])]
+generator2 = genTuples combinations3digit
 
 x_generator2 :: Int
 x_generator2 =
-    length [ t | t <- ts , t ‘elem ‘ g ]
+    length [ t | t <- ts , t `elem` g ]
     where
     g = generator2
     ts =
@@ -53,3 +50,8 @@ x_generator2 =
         , (" 879","79","897 ","98","789 ")
         , (" 987","79","789 ","79","789 ")
         ]
+
+
+main :: IO ()
+main = 
+  print  x_generator2
